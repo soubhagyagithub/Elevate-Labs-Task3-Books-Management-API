f6e2e0018fd9d8c2114931982b7375143fe4b475
Soubhagya Ranjan Sethi <93478954+soubhagyagithub@users.noreply.github.com>
2025-05-30 18:05:40 +0530

    added cors to fix cors error

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const booksRoutes = require("./routes/booksRoutes");
 
 app.use(express.json());
+app.use(cors());
 app.use("/books", booksRoutes);
 
 const PORT = 3000;
